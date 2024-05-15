294769f8fe06d015032525adc012b9a2db2f5317
karamzyatina <67735224+karamzyatina@users.noreply.github.com>
2024-05-15 11:26:03 +0300

    Delete Node.cpp

--- Node.cpp
+++
@@ -1,4 +0,0 @@
-#include "Node.h"
-
-Node::Node(std::string department, std::string patientName, std::string arrivalDate, std::string dischargeDate, std::string diagnosis)
-    : department(department), patientName(patientName), arrivalDate(arrivalDate), dischargeDate(dischargeDate), diagnosis(diagnosis), next(nullptr) {}
