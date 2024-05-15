3f3a7af9bdd2c9f30794ac0e1c3ddbcff4310312
karamzyatina <67735224+karamzyatina@users.noreply.github.com>
2024-05-15 12:40:09 +0300

    Update main.cpp

--- 2/main.cpp
+++ 2/main.cpp
@@ -1,1 +1,17 @@
-123
+#include <iostream>
+#include "LinkedList.h"
+#include "Menu.h"
+
+int main() {
+    LinkedList<std::string> journal;
+
+    int choice;
+    do {
+        Menu::displayMenu();
+        choice = Menu::handleChoice(journal);
+    } while (choice != 6);
+    
+    std::cout << " Работу выполнил студент: Саблин Дмитрий Владимирович, ДБИ-101кр " << std::endl;
+    
+    return 0;
+}
